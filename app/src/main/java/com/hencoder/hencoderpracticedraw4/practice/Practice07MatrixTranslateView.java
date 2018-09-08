68bc6e523f4593e1c10c021ed88ded94c8c959f6
xiaweizi <1012126908@qq.com>
2018-09-08 15:27:20 +0800

    [dev] 自定义 View matrix

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice07MatrixTranslateView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice07MatrixTranslateView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.support.annotation.Nullable;
@@ -16,7 +17,8 @@ public class Practice07MatrixTranslateView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
-    Point point2 = new Point(600, 200);
+    Point point2 = new Point(300, 200);
+    Matrix matrix = new Matrix();
 
     public Practice07MatrixTranslateView(Context context) {
         super(context);
@@ -38,7 +40,18 @@ public class Practice07MatrixTranslateView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.save();
+        matrix.reset();
+        matrix.postTranslate(-20, -20);
+        canvas.concat(matrix);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        matrix.reset();
+        matrix.preTranslate(200, 0);
+        canvas.concat(matrix);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
